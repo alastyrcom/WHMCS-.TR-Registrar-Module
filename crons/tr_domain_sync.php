44512d9543e587a2fccf9938ea4bfb00fbbc8b12
alastyrcom <61419667+alastyrcom@users.noreply.github.com>
2020-02-25 15:24:23 +0300

    Update cron file

--- crons/tr_domain_sync.php
+++ crons/tr_domain_sync.php
@@ -24,38 +24,42 @@ foreach($waitingdomains as $domain){
     $postfields['domainName'] = $domain->domainname;
 
 
-  $laststate = alastyr_CheckDomainStatus($postfields);
+    $laststate = alastyr_CheckDomainStatus($postfields);
 
-  if($laststate['status'] == "7"){
-     $domaininfo =  Capsule::table('tbldomains')->where('domain', $postfields['domainName'])->first();
-     $order =  Capsule::table('tblorders')->where('id', $domaininfo->orderid)->first();
-     $nameservers = explode(',', $order->nameservers);
+    if($laststate['status'] == "7"){
+        $domaininfo =  Capsule::table('tbldomains')->where('domain', $postfields['domainName'])->first();
+        $order =  Capsule::table('tblorders')->where('id', $domaininfo->orderid)->first();
+        $nameservers = explode(',', $order->nameservers);
 
-      $command = 'DomainUpdateNameservers';
-      $postData = array(
-          'domainid' => $domaininfo->id,
-          'ns1' => ''.$nameservers[0].'',
-          'ns2' => ''.$nameservers[1].'',
-          'ns3' => ''.$nameservers[2].'',
-          'ns4' => ''.$nameservers[3].'',
-          'ns5' => ''.$nameservers[4].'',
-      );
-     $results = localAPI($command, $postData);
+        $command = 'DomainUpdateNameservers';
+        $postData = array(
+            'domainid' => $domaininfo->id,
+            'ns1' => ''.$nameservers[0].'',
+            'ns2' => ''.$nameservers[1].'',
+            'ns3' => ''.$nameservers[2].'',
+            'ns4' => ''.$nameservers[3].'',
+            'ns5' => ''.$nameservers[4].'',
+        );
+        $results = localAPI($command, $postData);
 
-      $command = 'SendEmail';
-      $postDatamail = array(
-          'messagename' => 'TR Alan Adı Basvuru Onayi',
-          'id' => $domaininfo->id,
-      );
-     $results = localAPI($command, $postDatamail);
+        $command = 'SendEmail';
+        $postDatamail = array(
+            'messagename' => 'TR Alan Adı Basvuru Onayi',
+            'id' => $domaininfo->id,
+        );
+        $results = localAPI($command, $postDatamail);
 
-      $command = 'UpdateClientDomain';
-      $postData = array(
-          'domainid' => $domaininfo->id,
-          'status' => 'Active',
-      );
-
-  }
+        $laststate = alastyr_DomainInfo($postfields);
+        $expdate = date("Y-m-d", substr($laststate['expirationDate'], 0, -3 ));
+        $command = 'UpdateClientDomain';
+        $postData = array(
+            'domainid' => $domaininfo->id,
+            'status' => 'Active',
+            'nextduedate' => $expdate,
+            'expirydate' => $expdate
+        );
+        $results = localAPI($command, $postDatamail);
+    }
 
     $logarray = array('domainid' => $domain->domainid, 'userid' => $domain->userid, 'domainname' => $domain->domainname, 'ticketid' => $laststate['ticketNumber'], 'laststatus' => $laststate['status'], 'actiontype' => $laststate['actionType'], 'actioncomment' => $laststate['actionComment'], 'lastdescription' => $laststate['detail']);
     alastyr_addDomainLog($logarray);
